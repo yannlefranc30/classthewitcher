6f96555b33bde4458d149575198afe4285438c1d
yann <yannlefranc330@gmail.com>
2023-04-17 17:58:50 +0200

    thewitcherclassprogrames

---
+++ src/thewitcher.java
@@ -0,0 +1,11 @@
+public class thewitcher {
+    public static void thewitcher(String[] args){
+
+        int thewitch = 9;
+        double thewitcher = thewitch; // Automatic casting: int to double
+
+        System.out.println(thewitch);
+        System.out.println(thewitcher);
+
+    }
+}
